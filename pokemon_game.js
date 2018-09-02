f04a9c29726cd2f2795e993e19bc8b1e05eb5d06
kearrooooooooooooooo <32694622+kearrooooooooooooooo@users.noreply.github.com>
2018-09-02 00:01:16 -0700

    yay got the basic stuff working
    yaaaaaaaay

--- pokemon_game.js
+++ pokemon_game.js
@@ -1,7 +1,7 @@
 var their_HP = 300;
 var my_HP = 300; 
 
-//their_attacks = [thunderbolt, quickAttack, thunder, chargeBeam];
+their_attacks = [thunderbolt, quickAttack, thunder, slam];
 
 var playerMove = 0; 
 
@@ -33,40 +33,6 @@ function ice_beam() {
 		}
 		}
 
-<<<<<<< HEAD
-function thunderbolt(){
-	var miss = Math.floor((Math.random() * 10) + 1); // miss rate
-	if(miss == 1) {
-		document.getElementById("message").innerHTML = "PIKACHU's attack missed!";
-	}
-	else{
-		document.getElementById('message').innerHTML = "PIKACHU used thunderbolt"	
-		var crit = Math.floor((Math.random() * 10) + 1);
-		if(crit == 4){
-			for(var x = 0; x < 2; x++){
-				my_HP -= 30;
-			}
-		}
-		else{
-			my_HP -=30;
-		}
-		if(my_HP < 0){my_HP =0}
-			document.getElementById("my_HP").innerHTML = my_HP;
-		if(my_HP == 0){
-			document.getElementById("message").innerHTML = "MUDKIP fainted!"
-		}
-	}
-}
-
-/*function comp_pokemon(){
-	if(playerMove == 1 & their_HP != 0){
-		//var moves = Math.floor((Math.random() * 4) + 1); their math is wrong lul
-		thunderbolt();
-		playerMove = 0;
-	}
-}
-*/
-=======
 function surf() {
 	if (playerMove == 0 & my_HP != 0) {
 		var miss = Math.floor((Math.random() * 10) + 1); // miss rate
@@ -152,4 +118,107 @@ function waterfall() {
 		}
 
 //enemy pokemon moves
->>>>>>> 45122942becfac9165591d99e9dd951e8b591f7e
+function thunderbolt(){
+	var miss = Math.floor((Math.random() * 10) + 1); // miss rate
+	if(miss == 1) {
+		document.getElementById("message").innerHTML = "PIKACHU's attack missed!";
+	}
+	else{
+		document.getElementById('message').innerHTML = "PIKACHU used thunderbolt"	
+		var crit = Math.floor((Math.random() * 10) + 1);
+		if(crit == 4){
+			for(var x = 0; x < 2; x++){
+				my_HP -= 30;
+			}
+		}
+		else{
+			my_HP -=30;
+		}
+		if(my_HP < 0){my_HP =0}
+			document.getElementById("my_HP").innerHTML = my_HP;
+		if(my_HP == 0){
+			document.getElementById("message").innerHTML = "MUDKIP fainted!"
+		}
+	}
+}
+
+function quickAttack(){
+	var miss = Math.floor((Math.random() * 10) + 1); // miss rate
+	if(miss == 1) {
+		document.getElementById("message").innerHTML = "PIKACHU's attack missed!";
+	}
+	else{
+		document.getElementById('message').innerHTML = "PIKACHU used quick attack"	
+		var crit = Math.floor((Math.random() * 10) + 1);
+		if(crit == 4){
+			for(var x = 0; x < 2; x++){
+				my_HP -= 30;
+			}
+		}
+		else{
+			my_HP -=30;
+		}
+		if(my_HP < 0){my_HP =0}
+			document.getElementById("my_HP").innerHTML = my_HP;
+		if(my_HP == 0){
+			document.getElementById("message").innerHTML = "MUDKIP fainted!"
+		}
+	}
+}
+
+function thunder(){
+	var miss = Math.floor((Math.random() * 10) + 1); // miss rate
+	if(miss == 1) {
+		document.getElementById("message").innerHTML = "PIKACHU's attack missed!";
+	}
+	else{
+		document.getElementById('message').innerHTML = "PIKACHU used thunder"	
+		var crit = Math.floor((Math.random() * 10) + 1);
+		if(crit == 4){
+			for(var x = 0; x < 2; x++){
+				my_HP -= 30;
+			}
+		}
+		else{
+			my_HP -=30;
+		}
+		if(my_HP < 0){my_HP =0}
+			document.getElementById("my_HP").innerHTML = my_HP;
+		if(my_HP == 0){
+			document.getElementById("message").innerHTML = "MUDKIP fainted!"
+		}
+	}
+}
+
+function slam(){
+	var miss = Math.floor((Math.random() * 10) + 1); // miss rate
+	if(miss == 1) {
+		document.getElementById("message").innerHTML = "PIKACHU's attack missed!";
+	}
+	else{
+		document.getElementById('message').innerHTML = "PIKACHU used slam"	
+		var crit = Math.floor((Math.random() * 10) + 1);
+		if(crit == 4){
+			for(var x = 0; x < 2; x++){
+				my_HP -= 30;
+			}
+		}
+		else{
+			my_HP -=30;
+		}
+		if(my_HP < 0){my_HP =0}
+			document.getElementById("my_HP").innerHTML = my_HP;
+		if(my_HP == 0){
+			document.getElementById("message").innerHTML = "MUDKIP fainted!"
+		}
+	}
+}
+
+function comp_pokemon(){
+	if(playerMove == 1 & their_HP != 0){
+		var moves = Math.floor((Math.random() * 4));
+		their_attacks[moves]();
+		playerMove = 0;
+	}
+}
+
